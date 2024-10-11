efa6affdf177bf167f5d78a6e6f41b70d315d8fa
Sojin <inailedit777@gmail.com>
2024-10-11 15:05:20 +0900

    2024-10-11 15:05

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Main from './views/Main';
+import AccommodationList from './views/accommodationlist';
+
 
 export default function App() {
   return (
     <Routes>
       <Route path='/main' element={<Main />}/>
+      <Route path='/accommodationList' element={<AccommodationList />}/>
+
+      
     </Routes>
 
   );
--- src/apis/dto/response/guset/auth/index.ts
+++
@@ -1,5 +0,0 @@
-import SignInResponseDto from "./g-sign-in.response.dto";
-
-export type {
-    SignInResponseDto 
-}
