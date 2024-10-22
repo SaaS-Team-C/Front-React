ab6c2feba852fb7b5638cf378238a5380f1a520f
returnseo <reseo4150@gmail.com>
2024-10-22 17:40:58 +0900

    okjinseo 10.22. 17:40

--- src/resources/information/index.ts
+++ src/resources/information/index.ts
@@ -1,26 +1,26 @@
 
 
 const Slider4Imformtion = [
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'},
-    {filter:'필터링', title:'제목', regon:'지역', propose:'추천수' , price:'가격'}
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'},
+    {filter:'필터링', title:'제목', Region:'지역', propose:'추천수' , price:'가격'}
 ]
 
 export {
