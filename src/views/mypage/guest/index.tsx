40caae0a3f9ede12883ab92f07f2f2a5876d0d53
returnseo <reseo4150@gmail.com>
2024-10-28 17:42:07 +0900

    okjinseo 24.10.28. 17:42

---
+++ src/views/mypage/guest/index.tsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import "./style.css"
+import Topbar from 'src/component/topbar'
+import MypageCatalogButton from 'src/component/mypage/mypagecatalogbutton';
+
+
+
+export default function GuestMypage() {
+
+    const [click, setClick] = useState<string>("내정보관리")
+
+    // event handler: 분류 버튼 클릭 이벤트 핸들러 //
+    const onClickButtonHandler = (distimction: string) => {
+        setClick(distimction);
+    }
+
+    const testValue = true ;
+
+
+  return (
+    <>
+    <Topbar/>
+    {<div id='mypage-wrapper'>
+        {testValue && <div className='guestMypage-side-bar'>
+            <MypageCatalogButton text='내정보관리' activite={click === '내정보관리' || click === ''} onClick={onClickButtonHandler} />
+            <MypageCatalogButton text='예약내역' activite={click === '예약내역' || click === ''} onClick={onClickButtonHandler} />
+            <MypageCatalogButton text='즐겨찾기' activite={click === '즐겨찾기' || click === ''} onClick={onClickButtonHandler} />
+            </div>}
+        {!testValue && <div className='hostMypage-side-bar'></div>}
+        {testValue && <div className='guestMypage-main'>
+            <div className='guestMypage-main-title'>
+                
+            </div>
+            <div className='guestMypage-main-detail'></div>
+            </div>}
+        {!testValue && <div className='hosttMypage-main'></div>}
+    </div>}
+    </>
+  )
+}
