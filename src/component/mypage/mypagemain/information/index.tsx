3622c84372272a11d43a7d35f305dbd1ea10fa9e
returnseo <reseo4150@gmail.com>
2024-10-29 09:39:46 +0900

    okjinseo mypage

--- src/component/mypage/mypagemain/information/index.tsx
+++ src/component/mypage/mypagemain/information/index.tsx
@@ -6,12 +6,12 @@ export default function Information() {
 
 
   return (
-    <div>
-        <div>
-            <div></div>
-            <div></div>
+    <div id='information-warpper'>
+        <div className='information-title'>
+            <div className='information-title-text'></div>
+            <div className='information-title-ditail'></div>
         </div>
-        <div></div>
+        <div className='information-main'></div>
     </div>
   )
 }
