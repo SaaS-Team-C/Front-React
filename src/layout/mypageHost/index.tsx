28c085b2a2efddc1f58a3a88e92eede4525e16f7
Sojin <inailedit777@gmail.com>
2024-11-01 17:46:35 +0900

    sojin 2024.11.01 17:46

--- src/layout/mypageHost/index.tsx
+++ src/layout/mypageHost/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Route, Routes, useNavigate, useLocation } from 'react-router-dom';
 import './style.css';
 import Topbar from 'src/component/topbar';
 import MypageCatalogButton from 'src/component/mypage/mypagecatalogbutton';
@@ -8,36 +9,43 @@ import { MyAccommodationManagement } from 'src/component/mypagehost/MyAccommodat
 
 
 export default function HostMypageLayout() {
+
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  // 현재 URL 경로에 맞춰 활성화된 탭을 설정
+  const currentPath = location.pathname;
+
+  // 네비게이션 버튼 클릭 핸들러
+  const onClickButtonHandler = (path: string) => {
+    navigate(path);  // path로 이동
+  };
+
   const datail1 = '내정보관리';
   const datail2 = '예약현황';
   const datail3 = '내 숙소정보 관리';
 
-  const [click, setClick] = useState<string>(datail1);
-  const userName = "이소진"; 
-
-  // 네비게이션 버튼 클릭 핸들러
-  const onClickButtonHandler = (distinction: string) => {
-    setClick(distinction);
-    
-  };
 
   return (
     <>
+    
       <Topbar />
 
-          {/* 상단 환영 문구 */}
-          <div id="welcome-message"> 호스트 '{userName}'님, 반갑습니다.</div>
+
             <div id="mypage-wrapper">
               <div className="guestMypage-side-bar">
-                <MypageCatalogButton text={datail1} activite={click === datail1} onClick={() => onClickButtonHandler(datail1)} />
-                <MypageCatalogButton text={datail2} activite={click === datail2} onClick={() => onClickButtonHandler(datail2)} />
-                <MypageCatalogButton text={datail3} activite={click === datail3} onClick={() => onClickButtonHandler(datail3)} />
+                <MypageCatalogButton text={datail1} activite={currentPath === "/mypagehost"} onClick={() => onClickButtonHandler("/mypagehost")} />
+                <MypageCatalogButton text={datail2} activite={currentPath === "/mypagehost/books"} onClick={() => onClickButtonHandler("/mypagehost/books")} />
+                <MypageCatalogButton text={datail3} activite={currentPath === "/mypagehost/accommodations"} onClick={() => onClickButtonHandler("/mypagehost/accommodations")}
+          />
               </div>
 
               <div className="guestMypage-main">
-                {click === datail1 && <MyInfoManagement />}
-                {click === datail2 && <ReservationStatus />}
-                {click === datail3 && <MyAccommodationManagement />}
+               <Routes>
+                <Route path="/mypagehost" element={<MyInfoManagement />} />
+                <Route path="/mypagehost/books" element={<ReservationStatus />} />
+                <Route path="/mypagehost/accommodations" element={<MyAccommodationManagement />} />
+              </Routes> 
               </div>
             </div>
     </>
