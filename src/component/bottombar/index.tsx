da6ea48957bb7eb18cefd563d2d7d88400463481
nayeon <tires0301@gmail.com>
2024-11-06 17:43:10 +0900

    nayeon 24.11.06 17:43

--- src/component/bottombar/index.tsx
+++ src/component/bottombar/index.tsx
@@ -22,23 +22,26 @@ export default function Bottombar() {
                     <div className="top-footer1-title">문의센터</div>
                     <div className="top-footer1-content1">카카오톡 문의: 오전 9시 ~ 새벽 3시 운영</div>
                     <div className="top-footer1-content2">FAQ: 24시간 운영</div>
-
                     <div className="top-footer1-button">
                         <button className='top-footer1-kakao' onClick={handleKakaoClick}>카카오톡 문의</button>
                         <button className="top-footer1-faq" onClick={handleFaqClick}>FAQ</button>
                     </div>
                 </div>
                 <div className="top-footer2">
-                    <div className="top-footer2-title"></div>
-                    <div className="top-footer2-content"></div>
+                    <div className="top-footer2-title">회사</div>
+                    <div className="top-footer2-content">회사소개</div>
                 </div>
                 <div className="top-footer3">
-                    <div className="top-footer3-title"></div>
-                    <div className="top-footer3-content"></div>
+                    <div className="top-footer3-title">인기 검색 키워드</div>
+                    <div className="top-footer3-content1">여행 추천 숙소</div>
+                    <div className="top-footer3-content2">국내 인기 여행지</div>
+                    
+
                 </div>
                 <div className="top-footer4">
-                    <div className="top-footer4-title"></div>
-                    <div className="top-footer4-content"></div>
+                    <div className="top-footer4-title">회원정보</div>
+                    <div className="top-footer4-content1">마이페이지</div>
+                    <div className="top-footer4-content2">로그아웃</div>
                 </div>
                 <div className="top-footer5">
                     <div className="top-footer5-title"></div>
